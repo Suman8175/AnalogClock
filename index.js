8396678e3bee973e489033c4aa900854088c3891
sumandevkota@gmail.com <sumandevkota75@gmail.com>
2023-08-01 21:34:43 +0545

    Analog Clock using HTML,CSS and JS

---
+++ index.js
@@ -0,0 +1,14 @@
+setInterval(()=>{
+var hour=new Date().getHours();
+var minute=new Date().getMinutes();
+var second=new Date().getSeconds();
+var Hourhand=document.getElementById("Hourhand");
+var Minutehand=document.getElementById("Minutehand");
+var secondhand=document.getElementById("secondhand");
+hrotate=30*hour+(minute/2)
+mrotate=6*minute;
+srotate=6*second
+Hourhand.style.transform=`rotate(${hrotate}deg)`;
+Minutehand.style.transform=`rotate(${mrotate}deg)`;
+secondhand.style.transform=`rotate(${srotate}deg)`;
+},1000)
